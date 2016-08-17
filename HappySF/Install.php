2ef9d76240c5b49e964e541e2472469d91bfa33e
kirrie <kirrie@gmail.com>
2016-08-17 11:04:44 +0900

    change cache directory
    change ../cache to ./cache

--- HappySF/Install.php
+++ HappySF/Install.php
@@ -5,7 +5,7 @@ class Install {
 	public static function postPackageInstall() {
 		if(strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN') {
 			// windows가 아닌 경우에만 cache 폴더의 권한을 777로 변경한다.
-			chmod('../cache', 0777);
+			chmod('./cache', 0777);
 		}
 	}
 }
