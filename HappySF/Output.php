c598058cd86a80b6479460b280393263e0fc50a9
kirrie <kirrie@gmail.com>
2016-08-17 10:53:30 +0900

    add output control
    - add output control using psr7 messaging interface
    - fix filesystem cache config

---
+++ HappySF/Output.php
@@ -0,0 +1,123 @@
+<?php
+namespace HappySF;
+
+use Symfony\Component\Yaml\Yaml;
+use GuzzleHttp\Psr7;
+use Zend\Cache;
+
+class Output {
+	protected $response = NULL;
+
+	public function __construct() {
+		$this->response = new Psr7\Response;
+		$this->response = $this->response->withBody(new Psr7\Stream(fopen('php://memory', 'r+')));
+		$this
+			->setStatusCode(200)
+			->setCacheMiss();
+	}
+
+	public function setStatusCode($statusCode) {
+		$this->response = $this->response->withStatus($statusCode);
+
+		return $this;
+	}
+
+	public function setContentType($contentType) {
+		$this->response = $this->response->withHeader('Content-Type', $contentType);
+
+		return $this;
+	}
+
+	public function setContentLength($contentLength) {
+		$this->response = $this->response->withHeader('Content-Length', $contentLength);
+
+		return $this;
+	}
+
+	public function setCacheHit($from = 'local') {
+		$this->response = $this->response->withHeader('X-Cache', 'HIT from ' . $from);
+
+		return $this;
+	}
+
+	public function setCacheMiss($from = 'local') {
+		$this->response = $this->response->withHeader('X-Cache', 'Miss from ' . $from);
+
+		return $this;
+	}
+
+	public function write($text) {
+		$this->response->getBody()->write($text);
+	}
+
+	public function cacheData($cacheKey, \Closure $callback) {
+		// for cache config
+		$config = getConfig();
+
+		// cache
+		$cache = Cache\StorageFactory::factory(array(
+			'adapter' => array(
+				'name' => 'filesystem',
+				'options' => array(
+					'ttl' => $config['cache']['ttl'],
+					'cache_dir' => BASE_PATH . '/cache'
+				)
+			),
+			'plugins' => array(
+				'exception_handler' => array(
+					'throw_exceptions' => FALSE
+				)
+			)
+		));
+		$data = $cache->getItem($cacheKey, $isCacheHit);
+		$this
+			->setContentType('application/rss+xml; charset=utf-8')
+			->setCacheHit();
+
+		if(!$isCacheHit) {
+			$data = $callback();
+			$cache->setItem($cacheKey, $data);
+			$this->setCacheMiss();
+		}
+
+		$this->write($data);
+	}
+
+	public function flush() {
+		// content-length
+		$body = $this->response->getBody();
+		$this->setContentLength($body->getSize());
+
+		if(!headers_sent()) {
+			// header part
+			header(sprintf(
+				'HTTP/%s %s %s',
+				$this->response->getProtocolVersion(),
+				$this->response->getStatusCode(),
+				$this->response->getReasonPhrase()
+			));
+
+			foreach($this->response->getHeaders() as $headerName => $headerValues) {
+				foreach($headerValues as $headerValue) {
+					header(sprintf('%s: %s', $headerName, $headerValue), FALSE);
+				}
+			}
+		}
+
+		// body part
+		if($body->getSize() !== 0) {
+			if($body->isSeekable()) {
+				$body->rewind();
+			}
+
+			while(!$body->eof()) {
+				echo $body->read(4096);
+
+				if(connection_status() != CONNECTION_NORMAL) {
+					break;
+				}
+			}
+		}
+	}
+}
+// EOF
--- index.php
+++ index.php
@@ -11,119 +11,64 @@ use GuzzleHttp as GuzzleHttp;
 use HappySF as HappySF;
 use Zend as Zend;
 
-// response
-$response = new GuzzleHttp\Psr7\Response;
-$response = $response
-	->withStatus(200)
-	->withHeader('X-Cache', 'MISS from local')
-	->withBody(new GuzzleHttp\Psr7\Stream(fopen('php://memory', 'r+')));
-$body = $response
-	->getBody();
+// config function
+function getConfig() {
+	static $config = NULL;
+
+	if(is_null($config)) {
+		$config = Yaml::parse(file_get_contents(BASE_PATH . '/config.yaml'));
+	}
+
+	return $config;
+}
+
+// output
+$output = new HappySF\Output;
 
 try {
-	// config
-	$config = Yaml::parse(file_get_contents(BASE_PATH . '/config.yaml'));
-
 	// use cached data
-	$cache_key = 'rss';
-	$cache = Zend\Cache\StorageFactory::factory(array(
-		'adapter' => array(
-			'name' => 'filesystem',
-			'ttl' => $config['cache']['ttl'],
-			'cache_dir' => BASE_PATH . '/cache'
-		),
-		'plugins' => array(
-			'exception_handler' => array(
-				'throw_exceptions' => FALSE
-			)
-		)
-	));
-	$cached_data = $cache->getItem($cache_key, $cache_hit);
-	if($cache_hit) {
-		$response = $response
-			->withHeader('Content-Type', 'application/rss+xml')
-			->withHeader('X-Cache', 'HIT from local');
-		$body
-			->write($cached_data);
-	}
+	$output->cacheData('rss', function() {
+		// config
+		$config = getConfig();
 
-	// http request
-	$client = new GuzzleHttp\Client();
-	$result = $client->request('GET', 'http://happysf.net/zeroboard/zboard1.php?id=reader');
+		// http request
+		$client = new GuzzleHttp\Client();
+		$result = $client->request('GET', 'http://happysf.net/zeroboard/zboard.php?id=reader');
 
-	// rss feed channel
-	$feed = new Zend\Feed\Writer\Feed;
-	$feed
-		->setTitle($config['feed']['title'])
-		->setLink($config['feed']['homepage_url'])
-		->setDescription($config['feed']['description'])
-		->setFeedLink($config['feed']['feed_url'], $config['feed']['type'])
-		->setDateModified(time())
-		->addAuthor($config['feed']['author']);
+		// rss feed channel
+		$feed = new Zend\Feed\Writer\Feed;
+		$feed
+			->setTitle($config['feed']['title'])
+			->setLink($config['feed']['homepage_url'])
+			->setDescription($config['feed']['description'])
+			->setFeedLink($config['feed']['feed_url'], $config['feed']['type'])
+			->setDateModified(time())
+			->addAuthor($config['feed']['author']);
 
-	$html = HtmlDomParser::str_get_html($result->getBody());
-	foreach($html->find('tr[bgcolor=ffffff]') as $element) {
-		$article = new HappySF\Article($element);
-		$feed->addEntry(
-			$feed->createEntry()
-				->setTitle($article->getTitle())
-				->setLink($article->getURL())
-				->addAuthor(array(
-					'name' => $article->getAuthor()
-				))
-				->setDateCreated($article->getDate())
-				->setDateModified($article->getDate())
-				->setContent($article->getContent())
-		);
-	}
+		$html = HtmlDomParser::str_get_html($result->getBody());
+		foreach($html->find('tr[bgcolor=ffffff]') as $element) {
+			$article = new HappySF\Article($element);
+			$feed->addEntry(
+				$feed->createEntry()
+					->setTitle($article->getTitle())
+					->setLink($article->getURL())
+					->addAuthor(array(
+						'name' => $article->getAuthor()
+					))
+					->setDateCreated($article->getDate())
+					->setDateModified($article->getDate())
+					->setContent($article->getContent())
+			);
+		}
 
-	$rss = $feed->export($config['feed']['type']);
-	$response = $response
-		->withHeader('Content-Type', 'application/rss+xml');
-	$body
-		->write($rss);
-
-	// cache it
-	$cache->setItem($cache_key, $rss);
+		return $feed->export($config['feed']['type']);
+	});
 } catch(GuzzleHttp\Exception\ClientException $e) {
-	$response = $response
-		->withStatus($e->getResponse()->getStatusCode());
-	$body
+	$output
+		->setStatusCode($e->getResponse()->getStatusCode())
 		->write($e->getMessage());
 }
 
 // flush
-$headers = $response->getHeaders();
-$body = $response->getBody();
-
-if(!headers_sent()) {
-	// header part
-	header(sprintf(
-		'HTTP/%s %s %s',
-		$response->getProtocolVersion(),
-		$response->getStatusCode(),
-		$response->getReasonPhrase()
-	));
-
-	foreach($headers as $header_name => $header_values) {
-		foreach($header_values as $header_value) {
-			header(sprintf('%s: %s', $header_name, $header_value), FALSE);
-		}
-	}
-}
-
-// body part
-if($body->getSize() !== 0) {
-	if($body->isSeekable()) {
-		$body->rewind();
-	}
-
-	while(!$body->eof()) {
-		echo $body->read(4096);
-
-		if(connection_status() != CONNECTION_NORMAL) {
-			break;
-		}
-	}
-}
+$output->flush();
 // EOF
